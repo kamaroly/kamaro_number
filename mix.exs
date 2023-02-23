7445d8f9143d4d40e968c02c95691cacf06deb61
Kamaro <kamaroly@gmail.com>
2023-02-23 19:52:57 +0300

    Updated mix.exs to meet publication requirements.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule KamaroNumber.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description(),
+      source_url: "https://github.com/kamaroly/kamaro_number"
     ]
   end
 
@@ -20,9 +23,21 @@ defmodule KamaroNumber.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
+    []
+  end
+
+  defp description() do
+    "A minimal package to convert numbers into Words such as one hundred twenty three point thirty seven instead of 123.37."
+  end
+
+  defp package() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # This option is only needed when you don't want to use the OTP application name
+      name: "kamaro_number",
+      # These are the default files included in the package
+      files: ~w(lib test .formatter.exs mix.exs README* readme*),
+      licenses: ["MIT License"],
+      links: %{"GitHub" => "https://github.com/kamaroly/kamaro_number"}
     ]
   end
 end
